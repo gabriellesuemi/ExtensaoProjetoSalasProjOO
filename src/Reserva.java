0a83e440a959420ec600b1e60800d5ac1b7f185a
gabriellesuemi <gabrielle.alencar@unifesp.br>
2026-05-14 21:39:23 -0300

    Implementação da classe Reserva

--- src/Reserva.java
+++ src/Reserva.java
@@ -1,3 +1,41 @@
 public class Reserva {
-    
+
+    private Usuario usuario;
+    private String sala;
+    private String horario;
+    private String status;
+
+    public Reserva(
+            Usuario usuario,
+            String sala,
+            String horario) {
+
+        this.usuario = usuario;
+        this.sala = sala;
+        this.horario = horario;
+        this.status="Criada";
+    }
+
+    public void cancelar() {
+        status="Cancelada";
+    }
+
+    public void alterarHorario(String novoHorario){
+        horario=novoHorario;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public ReservaMemento salvar(){
+
+        return new ReservaMemento(
+                "Sala: " + sala
+                + " | Horário: "
+                + horario
+                + " | Status: "
+                + status
+        );
+    }
 }

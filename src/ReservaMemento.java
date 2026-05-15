3f47573bc792021de3fae8212c8553f13fd48e40
gabriellesuemi <gabrielle.alencar@unifesp.br>
2026-05-14 21:41:10 -0300

    Implementação da classe Memento da reserva

--- src/ReservaMemento.java
+++ src/ReservaMemento.java
@@ -1,3 +1,12 @@
 public class ReservaMemento {
-    
+
+    private String descricao;
+
+    public ReservaMemento(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
